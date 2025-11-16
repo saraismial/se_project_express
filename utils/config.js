088b16ada82530916fdcdec17179cb039988e6b8
Sara Ismial <sara-ismial@outlook.com>
2025-11-16 16:30:42 -0600

    Fixing errors

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-const JWT_SECRET = "saras-super-secret-key-123";
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-module.exports = { JWT_SECRET };
+module.exports = { JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret' };
