2cd790b0e7f39acdd47f8455b3e0c31ccf9cbbc9
Sara Ismial <sara-ismial@outlook.com>
2025-11-16 14:56:12 -0600

    Fixing errors

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use(express.json());
 // enable cross origin resource sharing
 app.use(cors());
 
+// temporary hardcoded user for tests
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133',
+  };
+  next();
+});
+
 // public routes
 app.post('/signin', login);
 app.post('/signup', createUser);
